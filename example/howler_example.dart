5379eb06339838a7aa223ab0412ef2461f0f9b03
Graciliano M. Passos <gracilianomp@gmail.com>
2019-01-06 17:42:56 -0200

    Fix example

--- example/howler_example.dart
+++ example/howler_example.dart
@@ -1,6 +1,14 @@
 import 'package:howler/howler.dart';
 
 main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+
+  var howl = new Howl(
+      src: ['audio/track.mp3','audio/track.wav'], // source in MP3 and WAV fallback
+      loop: true,
+      volume: 0.60 // Play with 60% of original volume.
+  ) ;
+
+  howl.play(); // Play sound.
+  howl.fade(0.0, 0.60, 10000) ; // Make a fade, from volume 0% to 60% in 10s
+
 }
